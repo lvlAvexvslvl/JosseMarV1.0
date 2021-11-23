09705e64d6f3ab3439b45f451fa4eccb24523032
YeydelF <56987545+YeydelF@users.noreply.github.com>
2021-11-22 20:38:05 -0600

    Movimientos

---
+++ Datos/CDatosMovimientos.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace Datos
+{
+    public class CDatosMovimientos
+    {
+        private CConexion conexion = new CConexion();
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand(); 
+    CDatosConsulta c = new CDatosConsulta();
+        public string MovimientoVenta(int idVenta, double Total)
+        {
+            try
+            {
+                comando.Connection = conexion.AbrirConexion();
+                comando.CommandText = "JSMMovimientoVenta";
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@idVenta", idVenta);
+                comando.Parameters.AddWithValue("@Total", Total);
+                comando.ExecuteNonQuery();
+                comando.Parameters.Clear();
+
+
+                return "Guardado";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex + "Eroror");
+                return "ERROR3";
+            }
+
+        }
+    
+    public string MovimientoCompra(int idCompra, double Total)
+    {
+        try
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "JSMMovimientoCompra";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@idVenta", idCompra);
+            comando.Parameters.AddWithValue("@Total", Total);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+
+            return "Guardado";
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex + "Eroror");
+            return "ERROR3";
+        }
+
+    }
+
+public string AbrirCaja(double Total)
+{
+    try
+    {
+        comando.Connection = conexion.AbrirConexion();
+        comando.CommandText = "JSMAbrirCaja";
+        comando.CommandType = CommandType.StoredProcedure;
+        comando.Parameters.AddWithValue("@Total", Total);
+        comando.ExecuteNonQuery();
+        comando.Parameters.Clear();
+
+        return "Guardado";
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex + "Eroror");
+        return "ERROR3";
+    }
+
+}
+    
+  public string CerrarCaja()
+{
+    try
+    {
+        comando.Connection = conexion.AbrirConexion();
+        comando.CommandText = "JSMCerrarCaja";
+        comando.CommandType = CommandType.StoredProcedure;
+        comando.ExecuteNonQuery();
+        comando.Parameters.Clear();
+
+                string fecha = DateTime.Now.ToString("yyyy-MM-dd");
+        return c.CSimple("SELECT Movimientos.TotalFactura FROM Movimientos WHERE Movimientos.Fecha='"+fecha+"' AND Movimientos.tipoMovimiento = 4;");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex + "Eroror");
+        return "ERROR3";
+    }
+
+        }
+      
+    }
+}
---
+++ Logica/CLogicaMovimientos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+namespace Logica
+{
+    public class CLogicaMovimientos
+    {
+        CDatosMovimientos Mov = new CDatosMovimientos();
+
+        public string MovimientoVenta(string idVenta, string Total)
+        {
+            int idVenta2 = Convert.ToInt32(idVenta.Trim());
+            double tot = Convert.ToDouble(Total.Trim());
+            return Mov.MovimientoVenta(idVenta2, tot);
+        }
+
+        public string MovimientoCompras(string idCompra, string Total)
+        {
+            int idCompra2 = Convert.ToInt32(idCompra.Trim());
+            double tot = Convert.ToDouble(Total.Trim());
+            return Mov.MovimientoCompra(idCompra2, tot);
+        }
+
+        public string AbrirCaja(string Total)
+        {
+            double tot = Convert.ToDouble(Total.Trim());
+            return Mov.AbrirCaja( tot);
+        }
+
+        public string CerrarCaja()
+        {
+            return Mov.CerrarCaja();
+        }
+    }
+}
